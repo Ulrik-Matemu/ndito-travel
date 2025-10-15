df39d5c48265624bf7037c8f37f2543a1ca09426
Ulrik-Matemu <advancechronicles@gmail.com>
2025-10-15 22:21:51 +0300

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/shared/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "Ndito Travel",
@@ -25,6 +26,7 @@ export default function RootLayout({
           
         </div>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
